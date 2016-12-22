8c64db324b594b1334b321743d8a050612547788
Naveen Shaji <xsf.azrael@gmail.com>
2016-12-22 23:24:02 +0530

    near production

--- php/register.php
+++ php/register.php
@@ -8,7 +8,7 @@ $travel = $_POST['traveldetails'];
 $contact = $_POST['contact'];
 $eventslist = $_POST['eventslist'];
 
-$to = 'outlawed@nujs.edu, xsf.azrael@gmail.com';
+$to = 'outlawed17@gmail.com';
 $subject = 'New OUTLAWED Registration';
 $message = 'University: '.$name."\r\n".' No. of students: '.$number."\r\n".'No. of students who needs accomodation: '.$numberacc."\r\n".'Time of Arrival: '.$timearrival."\r\n".'Time of Departure: '.$timedeparture."\r\n".'Travel details: '.$travel."\r\n".'Contact: '.$contact."\r\n".'Events: '.$eventslist."\r\n";
 $headers = 'From: mail@nujsoutlawed.in' . "\r\n";
