99a9176df9d2a3d6bd2ce47a83ebd6f5f515d84b
Naveen Shaji <xsf.azrael@gmail.com>
2017-02-27 19:16:02 +0530

    stable

--- php/register.php
+++ php/register.php
@@ -7,10 +7,15 @@ $timedeparture = $_POST['timeanddatedeparture'];
 $travel = $_POST['traveldetails'];
 $contact = $_POST['contact'];
 $eventslist = $_POST['eventslist'];
+$selectedChecks  = 'None';
+if(isset($_POST['checkboxes']) && is_array($_POST['checkboxes']) && count($_POST['checkboxes']) > 0){
+    $selectedProjects = implode(', ', $_POST['checkboxes']);
+}
+
 
 $to = 'outlawed17@gmail.com';
 $subject = 'New OUTLAWED Registration';
-$message = 'University: '.$name."\r\n".' No. of students: '.$number."\r\n".'No. of students who needs accomodation: '.$numberacc."\r\n".'Time of Arrival: '.$timearrival."\r\n".'Time of Departure: '.$timedeparture."\r\n".'Travel details: '.$travel."\r\n".'Contact: '.$contact."\r\n".'Events: '.$eventslist."\r\n";
+$message = 'University: '.$name."\r\n".' No. of students: '.$number."\r\n".'No. of students who needs accomodation: '.$numberacc."\r\n".'Time of Arrival: '.$timearrival."\r\n".'Time of Departure: '.$timedeparture."\r\n".'Travel details: '.$travel."\r\n".'Contact: '.$contact."\r\n".'Events: '.$eventslist."\r\n".'Selected Checkboxes: ' . $selectedChecks;
 $headers = 'From: mail@nujsoutlawed.in' . "\r\n";
 
 
